22236500532f4707ad9c11a64d3710b583700f83
chaitanyap14 <cpalghadmal14gmail.com>
2020-11-27 13:24:16 +0530

    portfolio

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,9 +1,9 @@
-const navSlide = () => {
-    const burger = document.querySelector(`.burger`);
-    const nav = document.querySelector(`.nav-links`);
+const navSlide = ()=> {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
 
-    burger.addEventListener(`click`, () => {
-        nav.classList.toggle(`.nav-active`);
+    burger.addEventListener('click', ()=> {
+        nav.classList.toggle('nav-active');
     });
 }
 
